c1c2bfb14adeccf681c43a54275927b4c5598304
Kel Nguyen <khanhntd@amazon.com>
2024-08-28 16:02:35 -0400

    Add Read me docs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MSCS532_Assignment1
+
+## Setup
+- **Step 1:** Download python https://www.python.org/downloads/
+- **Step 2:** Download python extension from vsc https://code.visualstudio.com/
+
+## Output
+- When running the result with `python3 main.py`, ![the result would be](image.png)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Step 1: Detach the rightest element
 # Step 2: Swap the each element from the current element's left side and swap it with the smallest element
 # (since we are doing decreasing order)
+# Time complexity: O(n^2) (O(n) when it is already sorted but worst case when the array is in the ascesding order and no duplicate elements)
+# Space complexity: O(1)
+# https://en.wikipedia.org/wiki/Insertion_sort
 def insertionSort(array: list[int]):
   for index in range(1, len(array)):
     key = array[index]
@@ -13,10 +16,10 @@ def insertionSort(array: list[int]):
       j -=1
     array[j+1] = key
 
-
-def printArray(arr):
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+# printArray will print all the elements in the array
+def printArray(array: list[int]):
+    for i in range(len(array)):
+        print(array[i], end=" ")
 
 if __name__ =="__main__":
   arr = [12, 11, 13, 5, 6]
